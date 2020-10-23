7b427958e859d7989bc74ba28e43598ae36d19c5
Jefferson gomes <jefferson.silva@medral.com.br>
2020-10-22 23:58:02 -0300

    PR: Usando Razor Layout

--- LojaVirtual/LojaVirtual/Startup.cs
+++ LojaVirtual/LojaVirtual/Startup.cs
@@ -52,7 +52,7 @@ namespace LojaVirtual
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Produto}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
